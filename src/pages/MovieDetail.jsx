e27195e1a4594e22746718d688d36007beeb384b
katerinafaustenova <katerina.faustenova@seznam.cz>
2023-11-25 19:48:01 -0700

    styling (wip)

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router";
 import { Link } from "react-router-dom";
 import { apikey, baseURL } from "../components/MoviesSearch";
+import styles from "./MovieDetail.module.css";
 
 function MovieDetail() {
   const [movie, setMovie] = useState({});
@@ -27,7 +28,7 @@ function MovieDetail() {
     fetchFn();
   }, [id]);
 
-  if (!movie)
+  if (!movie || Object.entries(movie).length === 0)
     return <p>There are no movie details we can display right now.</p>;
   if (error.isError) return <p>{error.msg}</p>;
 
@@ -35,6 +36,7 @@ function MovieDetail() {
     Poster,
     Title,
     Year,
+    Type,
     Language,
     Actors,
     Plot,
@@ -45,26 +47,50 @@ function MovieDetail() {
     imdbVotes,
   } = movie;
 
+  const ReleaseDate = new Date(Released);
+
   return (
-    <main>
-      <h1 className="title">
-        {Title}, {Year}
-      </h1>
-      <div>
-        <img src={Poster} alt="Poster" />
-        <p>Language: {Language}</p>
-        <p>Actors: {Actors}</p>
-        <p>Description: {Plot}</p>
-        <p>
-          Writer: {Writer}, Director: {Director}
-        </p>
-        <p>Released: {Released}</p>
-        <p>
-          IMDB Rating: {imdbRating}, Votes: {imdbVotes}
-        </p>
-      </div>
+    <main className={styles.main}>
+      <section className={styles.header}>
+        <Link to={`/`} className={styles.back}>
+          Zpět
+        </Link>
+        <h1 className={styles.title}>
+          {Title}, {Year}
+        </h1>
+      </section>
+      <section className={styles.content}>
+        <img src={Poster} alt="Poster" className={styles.image} />
+        <div className={styles.text}>
+          <p>
+            <span className={styles.label}>Type:</span> {Type}
+          </p>
+          <p>
+            <span className={styles.label}>Language:</span> {Language}
+          </p>
+          <p>
+            <span className={styles.label}>Actors:</span> {Actors}
+          </p>
+          <p>
+            <span className={styles.label}>Description:</span> {Plot}
+          </p>
+          <p>
+            <span className={styles.label}>Writer:</span> {Writer}
+          </p>
+          <p>
+            <span className={styles.label}>Director:</span> {Director}
+          </p>
+          <p>
+            <span className={styles.label}>Released:</span>{" "}
+            {ReleaseDate.toLocaleDateString()}
+          </p>
+          <p>
+            <span className={styles.label}>IMDB Rating:</span> {imdbRating},{" "}
+            <span className={styles.label}>Votes:</span> {imdbVotes}
+          </p>
+        </div>
+      </section>
       {/* TODO mam pri prechodu zpet ulozit predchozi data hledani ? */}
-      <Link to={`/`}>Zpět</Link>
     </main>
   );
 }
